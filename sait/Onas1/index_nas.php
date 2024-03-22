849beb65cf3d8cf28823bbaa101d72919ffa93cc
Mark <99024063+MarkXDD@users.noreply.github.com>
2024-03-22 11:53:28 +0300

    Add files via upload

---
+++ sait/Onas1/index_nas.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>О нас</title>
+</head>
+
+<body>
+    <header>
+       
+            <h1>Мастерская по пошиву спецодежды</h1>
+        <nav>
+            <ul>
+                <li><a href="http://marksait">назад</a></li>
+                
+            
+            </ul>
+
+        </nav>
+    </header>
+
+    <main>
+        
+
+        <section>
+            <h2>О нас</h2>
+            <p>Мы - команда опытных портных, которые специализируются на пошиве спецодежды. Мы стремимся предоставить нашим клиентам
+                продукты высокого качества и уникальный дизайн, соответствующий их потребностям.</p>
+        </section>
+
+        
+        <footer>
+        <p>© 2022 Мастерская по пошиву спецодежды. Все права защищены.</p>
+    </footer>
+    
+
+
+
+</body>
+
+</html>
---
+++ sait/marksait/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мастерская по пошиву спецодежды</title>
+</head>
+
+<body>
+    <header>
+        <h1>Мастерская по пошиву спецодежды</h1>
+        <nav>
+            <ul>
+                
+                <li><a href="http://onas1/index_nas.php">О нас</a></li>
+                <li><a href="http://services/index_services.php">Услуги</a></li>
+                <li><a href="http://portfolio/index_portfolio.php">Портфолио</a></li>
+                <li><a href="http://contacts/index_contacts.php">Контакты</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <h2>Главная</h2>
+            <p>Добро пожаловать в нашу мастерскую по пошиву спецодежды. Мы предлагаем высококачественный и индивидуальный пошив
+                специальной одежды для различных профессий.</p>
+        </section>
+
+     
+    </main>
+
+    <footer>
+        <p>© 2022 Мастерская по пошиву спецодежды. Все права защищены.</p>
+    </footer>
+
+
+    
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
